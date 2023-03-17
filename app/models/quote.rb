c8fc518ae6a2874f293a017647e4c7f0528a877b
Papot Alexandre <alexandre.papot@rethink-it.fr>
2023-03-17 18:18:19 +0100

    initial depot

---
+++ app/models/quote.rb
@@ -0,0 +1,15 @@
+# app/models/quote.rb
+
+class Quote < ApplicationRecord
+  has_many :line_item_dates, dependent: :destroy
+  validates :name, presence: true
+  belongs_to :company
+
+  scope :ordered, -> { order(id: :desc) }
+
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+  # Those three callbacks are equivalent to the following single line
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+# config/routes.rb
+
+Rails.application.routes.draw do
+  root to: "pages#home"
+  devise_for :users
+  resources :quotes
+
+  resources :quotes do
+    resources :line_item_dates, except: [:index, :show]
+  end
+
+end
